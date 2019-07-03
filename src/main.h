5e9d930796efedf95e378715ca29a90de43cf1ba
watanabe.ryuichi <jpz2046@linecorp.com>
2019-07-03 11:19:26 +0900

    add routing func

--- src/main.h
+++ src/main.h
@@ -8,15 +8,34 @@
 #include <string.h>
 #include <unistd.h>
 #include <errno.h>
+#include <sys/stat.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/syscall.h>
 
+#define MAXPENDING 5
 #define HTTP_TCP_PORT 80
 #define HTTPS_TCP_PORT 443
+#define _XOPEN_SOURCE 600
+#define _POSIX_C_SOURCE 200112L
+#undef _REENTRANT
+#define _GNU_SOURCE
 
 #ifndef __MAIN
 typedef struct {
     int port;
     int addr;
 } opt_p;
+
+typedef struct {
+        char method[16];
+        char path[128];
+        char version[16];
+} HEAD_DATA;
+
+struct ThreadArgs
+{
+    int clntSock;                      /* Socket descriptor for client */
+};
 #endif
 #define __MAIN 1
-
--- src/route.c
+++ src/route.c
@@ -1,5 +1,27 @@
 #include "route.h"
 
+/*
+struct file *sock_alloc_file(struct socket *sock, int flags, const char *dname)
+{
+	struct file *file;
+
+	if (!dname)
+		dname = sock->sk ? sock->sk->sk_prot_creator->name : "";
+
+	file = alloc_file_pseudo(SOCK_INODE(sock), sock_mnt, dname,
+				O_RDWR | (flags & O_NONBLOCK),
+				&socket_file_ops);
+	if (IS_ERR(file)) {
+		sock_release(sock);
+		return file;
+	}
+
+	sock->file = file;
+	file->private_data = sock;
+	return file;
+}
+*/
+
 char *routing(char *uri)
 {
     char *ret;
@@ -11,4 +33,47 @@ char *routing(char *uri)
 
     ret = uri + 1;
     return ret;
+}
+
+void cutcrLf( char *str )
+{
+        char *p;
+
+        if     ((p = strchr(str, '\r')) != NULL) *p = '\0';
+        else if((p = strchr(str, '\n')) != NULL) *p = '\0';
+}
+
+void gethtml( FILE *sock_fp, HEAD_DATA *data )
+{
+        FILE *fp;
+        char *buf;
+        struct stat st;
+
+        if(stat(data->path + 1, &st) < 0){
+                fprintf(stderr, "404error\n");
+                exit(1);
+        }
+
+    if((buf = (char *)calloc(st.st_size, sizeof(char))) == NULL){
+          fprintf(stderr, "calloc\n");
+          exit(1);
+    }
+
+        fprintf(sock_fp, "%s 200 OK\r\n", data->version);
+        fprintf(sock_fp, "Server:sample\r\n");
+        fprintf(sock_fp, "Content-Type:text/html\r\n");
+        fprintf(sock_fp, "Content-Length:%d\r\n", (int)st.st_size);
+        fprintf(sock_fp, "\r\n");
+
+        if((fp = fopen(data->path + 1, "r")) == NULL){
+                fprintf(stderr, "fopen\n");
+                exit(1);
+        }
+
+    fread(buf, 1, st.st_size, fp);
+    fwrite(buf, 1, st.st_size, sock_fp);
+
+        free(buf);
+        fclose(fp);
+
 }
